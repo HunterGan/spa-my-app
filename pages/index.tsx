9ad5148aab87598eac747b3202fa0fb3e8e04b37
HanterGan <gonidzze@gmail.com>
2023-02-08 13:18:07 +0300

    Adding components: Button, H, P

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,14 @@
-import { Button, Htag } from '@/components';
+import { Button, Htag, P } from '@/components';
 
 export default function Home(): JSX.Element {
   return (
     <>
       <Htag tag='h1'>Text</Htag>
-      <Button appearance='primary'>Button</Button>
-      <Button appearance='ghost'>Button</Button>
+      <Button appearance='primary' arrow='right'>Button</Button>
+      <Button appearance='ghost' arrow='down'>Button</Button>
+      <P size={'s'}>Text1</P>
+      <P size={'m'}>Text1</P>
+      <P size={'l'}>Text1</P>
     </>
   );
 }
