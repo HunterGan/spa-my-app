99d0c5bcf276a71b11ec3d7ab889f0f549adeb04
HanterGan <gonidzze@gmail.com>
2023-02-10 18:27:36 +0300

    Add Layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,8 @@
 import { Button, Htag, P, Tag, Rating} from '@/components';
+import { Layout, withLayout } from '../layout/Layout';
 import { useEffect, useState } from 'react';
 
-export default function Home(): JSX.Element {
+function Home(): JSX.Element {
   const [rating, setRating] = useState<number>(4);
   useEffect(() => {
     console.log('Counter');
@@ -29,3 +30,5 @@ export default function Home(): JSX.Element {
     </>
   );
 }
+
+export default withLayout(Home);
